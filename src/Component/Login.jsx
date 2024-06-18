c30aa04cb6258c9a11f9cfc18fda8e0bc8c26b00
Kuljot Singh <kuljotsingh0511@gmail.com>
2024-06-18 18:50:16 +0530

    react ecommerce start

---
+++ src/Component/Login.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+const Login = () =>
+    {
+        return (
+            <>
+      <div className="container">
+      <form className="form-Group" action="Login.jsx">
+         <h3 className="Signup-start">Signup</h3>
+      
+            
+                <label> <i class="fa-solid fa-user"></i></label>
+                <input type="text" placeholder="Your Name" id="name" required/><br/>
+                <label> <i class="fa-solid fa-envelope"></i></label>
+                <input type="email" placeholder="Your Email" id="email" required/><br/>
+                <label><i class="fa-solid fa-lock"></i></label>
+                <input type="password" placeholder="Enter Your Password" id="password" required/><br/>
+                <label><i class="fa-solid fa-lock-open"></i></label>
+                <input type="text" placeholder="Comform Your Password" id="password" required/><br/>
+                <input className="form-check-input" type="checkbox" required/>
+                <span className="term">Accept All Term And Condition</span> <br/>
+                <button type="submit" className="btn btn-primary btn1">Signup</button>
+               <a href="Login.jsx"><button type="button" className="btn btn-primary btn2">Login</button></a>
+            </form>
+         </div>
+         </>
+        )
+    }
+    export default Login;   
--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Signup from './Component/Signup';
+import Login from './Component/Login';
+import { Route, Routes } from 'react-router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <>
     <App />
+    <Signup/>
+    <Login/>
+    </>
   </React.StrictMode>
 );
+<browserRouter>
+<Routes>
+  <Route>
+    
+  </Route>
+</Routes>
+</browserRouter>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
